3f68e22a3351efbf62872773292a06c923d98e12
Gabriel-JM <gabriel.josemelo@hotmail.com>
2021-01-26 13:51:15 -0300

    Removing unnecessary code

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,5 @@
-const path = require('path')
-const fs = require('fs')
 const getCliOptions = require('./get-cli-options')
 const filesServer = require('../index')
-const Logger = require('../lib/logger')
 
 const [_, __, ...argvs] = process.argv
 
@@ -14,14 +11,7 @@ if(argvs.length) {
 
 const options = getCliOptions(argvs)
 
-const fullPath = path.resolve(pathToFile)
-const existsFile = fs.existsSync(path.join(fullPath, 'index.html'))
-
-if(existsFile) {
-  filesServer({
-    sourceFolder: pathToFile,
-    spa: options.spa
-  }).listen(options.port)
-} else {
-  Logger.cliPathNotFound(fullPath)
-}
+filesServer({
+  sourceFolder: pathToFile,
+  spa: options.spa
+}).listen(options.port)
